e6936236077a3d4ffc942b7686dbeb470a6a071e
Richard <zx980405@gmail.com>
2020-07-28 17:10:29 +0800

    modified to see github user

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<br></br>
+          Modified 1: checking github users
         </p>
         <a
           className="App-link"
