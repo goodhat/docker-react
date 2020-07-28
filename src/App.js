d07fa18ccc9105320cf0f1a0bf286499f95dfe38
Richard <zx980405@gmail.com>
2020-07-28 18:41:41 +0800

    add deploy setup

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.<br></br>
-          Modified 1: checking github users
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
